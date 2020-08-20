8942c46beefa1410b00b5125cb3a94ed47e65480
Viktor <surokv@gmail.com>
2020-08-20 18:48:32 +0300

    Initial commit

---
+++ WorkWithFiles/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WorkWithFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.buttonCreateFolded = new System.Windows.Forms.Button();
+            this.textBoxSearch = new System.Windows.Forms.TextBox();
+            this.buttonRemove = new System.Windows.Forms.Button();
+            this.buttonRemoveFolded = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.richTextBoxForSearched = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(12, 12);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(100, 30);
+            this.buttonCreate.TabIndex = 0;
+            this.buttonCreate.Text = "Create 100";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // buttonCreateFolded
+            // 
+            this.buttonCreateFolded.Location = new System.Drawing.Point(118, 12);
+            this.buttonCreateFolded.Name = "buttonCreateFolded";
+            this.buttonCreateFolded.Size = new System.Drawing.Size(100, 30);
+            this.buttonCreateFolded.TabIndex = 1;
+            this.buttonCreateFolded.Text = "Create Folded";
+            this.buttonCreateFolded.UseVisualStyleBackColor = true;
+            this.buttonCreateFolded.Click += new System.EventHandler(this.buttonCreateFolded_Click);
+            // 
+            // textBoxSearch
+            // 
+            this.textBoxSearch.Location = new System.Drawing.Point(12, 84);
+            this.textBoxSearch.Name = "textBoxSearch";
+            this.textBoxSearch.Size = new System.Drawing.Size(100, 20);
+            this.textBoxSearch.TabIndex = 2;
+            // 
+            // buttonRemove
+            // 
+            this.buttonRemove.Location = new System.Drawing.Point(12, 48);
+            this.buttonRemove.Name = "buttonRemove";
+            this.buttonRemove.Size = new System.Drawing.Size(100, 30);
+            this.buttonRemove.TabIndex = 3;
+            this.buttonRemove.Text = "Remove 100";
+            this.buttonRemove.UseVisualStyleBackColor = true;
+            this.buttonRemove.Click += new System.EventHandler(this.buttonRemove_Click);
+            // 
+            // buttonRemoveFolded
+            // 
+            this.buttonRemoveFolded.Location = new System.Drawing.Point(118, 48);
+            this.buttonRemoveFolded.Name = "buttonRemoveFolded";
+            this.buttonRemoveFolded.Size = new System.Drawing.Size(100, 30);
+            this.buttonRemoveFolded.TabIndex = 4;
+            this.buttonRemoveFolded.Text = "Remove Folded";
+            this.buttonRemoveFolded.UseVisualStyleBackColor = true;
+            this.buttonRemoveFolded.Click += new System.EventHandler(this.buttonRemoveFolded_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(118, 84);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(100, 20);
+            this.buttonSearch.TabIndex = 5;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // richTextBoxForSearched
+            // 
+            this.richTextBoxForSearched.Location = new System.Drawing.Point(12, 110);
+            this.richTextBoxForSearched.Name = "richTextBoxForSearched";
+            this.richTextBoxForSearched.Size = new System.Drawing.Size(206, 271);
+            this.richTextBoxForSearched.TabIndex = 6;
+            this.richTextBoxForSearched.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 393);
+            this.Controls.Add(this.richTextBoxForSearched);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonRemoveFolded);
+            this.Controls.Add(this.buttonRemove);
+            this.Controls.Add(this.textBoxSearch);
+            this.Controls.Add(this.buttonCreateFolded);
+            this.Controls.Add(this.buttonCreate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.Button buttonCreateFolded;
+        private System.Windows.Forms.TextBox textBoxSearch;
+        private System.Windows.Forms.Button buttonRemove;
+        private System.Windows.Forms.Button buttonRemoveFolded;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.RichTextBox richTextBoxForSearched;
+    }
+}
+
---
+++ WorkWithFiles/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WorkWithFiles
+{
+    public partial class Form1 : Form
+    {
+        private static string root = "C:/TestDirRoot";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Directory.CreateDirectory($"{root}/Folder_{i}");
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Directory.Delete($"{root}/Folder_{i}");
+            }
+        }
+
+        private void buttonCreateFolded_Click(object sender, EventArgs e)
+        {
+            string path = root;
+            for (int i = 0; i < 100; i++)
+            {
+                try
+                {
+                    path += $"/Folder_{i}";
+                    Directory.CreateDirectory(path);
+                }
+                catch (PathTooLongException)
+                {
+                    MessageBox.Show($"Path too long on step {i}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    break;
+                }
+            }
+        }
+
+        private void buttonRemoveFolded_Click(object sender, EventArgs e)
+        {
+            Directory.Delete($"{root}/Folder_0", true);
+        }
+
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            string fullFileName;
+            if ((fullFileName = SearchForFile(root, textBoxSearch.Text)) != null)
+            {
+                richTextBoxForSearched.BackColor = Color.Aquamarine;
+                richTextBoxForSearched.Text = OpenReadFile(fullFileName);
+            }
+            else
+            {
+                richTextBoxForSearched.BackColor = Color.LightPink;
+                richTextBoxForSearched.Text = "File was not found!";
+            }
+        }
+
+        /// <summary>
+        /// Recursive method to find the file with given name
+        /// </summary>
+        /// <param name="root">Starting directory</param>
+        /// <param name="fileName">File name</param>
+        /// <returns>Full path to the file, if file is present in current directory or subdirectories, null otherwise</returns>
+        private string SearchForFile(string root, string fileName)
+        {
+            string result;
+            //check if current directory contains the file
+            if ((result = Directory.GetFiles(root).FirstOrDefault(f => f.EndsWith($"/{fileName}"))) != null)
+            {
+                return result;
+            }
+
+            //if not, applying itself for each subdirectory
+            foreach (var directory in Directory.GetDirectories(root))
+            {
+                if ((result = SearchForFile(directory, fileName)) != null)
+                {
+                    return result;
+                }
+            }
+
+            //if none of the subdirectories returned filename, returning null
+            return null;
+        }
+
+        /// <summary>
+        /// Opens a file with text stream and reads it
+        /// </summary>
+        /// <param name="fileName">Full path to the file</param>
+        /// <returns>Content of the file</returns>
+        private string OpenReadFile(string fileName)
+        {
+            using (StreamReader sr = new StreamReader(new FileStream(fileName, FileMode.Open)))
+            {
+                return sr.ReadToEnd();
+            }
+        }
+    }
+}
